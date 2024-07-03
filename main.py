af3dbb7f8b815f3e2580274fa77e7ae6c447e8fc
kim <kmjun510@naver.com>
2024-07-03 11:24:08 +0900

    마스터 커밋1
    기본 코드 작성 완료

---
+++ main.py
@@ -0,0 +1,11 @@
+import foo import add_func #푸야, foo.py 만들고, 더하기 기능 완성해줘. 함수는 add_func()로 하고 return은 정수, parameter는 정수 두개
+import master import sub_func #마스터야. master.py 만들고, 빼기 기능 완성해줘.
+
+##변수부
+num1, num2, res=100, 200, 0
+
+#main
+res=add_func(num1, num2)
+print(res)
+res=sub func(num1, num2)
+print(res)
